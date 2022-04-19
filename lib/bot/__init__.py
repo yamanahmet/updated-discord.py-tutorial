94b33c33abed415bc0b1d384c81fe356e883834b
Ahmet YAMAN <64093249+yamanahmet@users.noreply.github.com>
2022-04-20 00:21:34 +0300

    Handling errors

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from discord import Intents
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord import Embed, File
 from discord.ext.commands import Bot as BotBase
+from discord.ext.commands import CommandNotFound
 
 PREFIX = "+"
 OWNER_IDS = [963358996994097202]
@@ -16,7 +19,7 @@ class Bot(BotBase):
 
         super().__init__(
             command_prefix=PREFIX,
-            owner_ids=OWNER_IDS
+            owner_ids=OWNER_IDS,
             intents=Intents.all(),
 
         )
@@ -36,11 +39,50 @@ class Bot(BotBase):
     async def on_disconnect(self):
         print("bot disconnected")
 
+    async def on_error(self, err, *args, **kwargs):
+        if err == "on_command_error":
+            await args[0].send("Someting went wrong.")
+
+        channel = self.get_channel(966019947241308292)
+        await channel.send("An error occured.")
+        raise
+
+    async def on_command_error(self, ctx, exc):
+        if isinstance(exc, CommandNotFound):
+            pass
+
+        elif hasattr(exc, "original"):
+            raise exc.original
+
+        else:
+            raise exc
+
     async def on_ready(self):
         if not self.ready:
             self.ready = True
             self.guild = self.get_guild(742767786635296810)
             print("bot ready")
+
+            channel = self.get_channel(966019947241308292)
+            await channel.send("Now online!")
+
+            embed = Embed(title="Now online!", description="Sequence is now online.",
+                          colour=0xFF0000, timestamp=datetime.utcnow())
+            fields = [("Name","Value", True),
+                      ("Another fields", "This field is next to the other one.", True),
+                      ("A non-inline field", "This Field will appear on it's own row.", False)]
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+            embed.set_author(name="Sequence Tutorials", icon_url=self.guild.icon_url)
+            embed.set_footer(text="This is a footer")
+            embed.set_thumbnail(url=self.guild.icon_url)
+            embed.set_image(url=self.guild.icon_url)
+            await channel.send(embed=embed)
+
+            await channel.send(file=File("./data/images/profile.png"))
+
+
+
 
         else:
             print("bot reconnected")
